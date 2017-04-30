9df85d7c1717871ebc60f43614bf5d3402cbaf4a
kuldeep3020 <kuldeepcuhp@gmail.com>
2017-05-01 00:49:18 +0530

    notification API implemented

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Notification } from '../../providers/notification';
 
 @Component({
   selector: 'page-contact',
-  templateUrl: 'contact.html'
+  templateUrl: 'contact.html',
+   providers: [Notification]
 })
 export class ContactPage {
    menu = false;
-  constructor(public navCtrl: NavController) {
-
+   public notificationss: any;
+   
+  constructor(public navCtrl: NavController , public notification: Notification) {
+this.notification.load().subscribe(data=> { this.notificationss = data });
   }
+   
   menu_overlay(event){ 
     this.menu = true;
   }
---
+++ src/providers/notification.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the CountryService provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class Notification {
+
+  constructor(public http: Http) {
+  }
+ load() {
+  return  this.http.get('http://devlsa.paulson.co.in/?q=apiv1/notification')
+      .map(res => res.json());
+ }
+}
